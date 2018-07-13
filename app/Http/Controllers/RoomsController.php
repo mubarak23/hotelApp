6a9327492d1970fcef3680f995ab5a2c847c81bd
Mubarak Aminu <mubarakaminu340@gmail.com>
2018-07-13 23:37:49 +0100

    check for avaliable rooms completed

--- app/Http/Controllers/RoomsController.php
+++ app/Http/Controllers/RoomsController.php
@@ -3,14 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Client;
+use App\Room;
 
 class RoomsController extends Controller
 {
     //
 
-    public function checkAvailableRooms(){
+    public function checkAvailableRooms(Request $request, $client_id){
 
-    	return view('rooms/checkAvailableRooms');
+    	$data = $request->all();
+
+    	$dateFrom = $request->input('dateFrom');
+    	$dateTo = $request->input('dateTo');
+
+    	$client = new Client();
+    	$room = new Room();
+
+    	$data = [];
+    	$data['room_id'] = $room->id;
+    	$data['client_id'] = $client_id;   	
+    	$data['dateFrom'] = $dateFrom;
+    	$data['dateTo'] = $dateTo;
+
+    	$data['rooms'] = $room->getAvailableRooms($dateFrom, $dateTo);
+
+    	$data['client'] = $client->find($client_id);
+
+
+
+    	return view('rooms/checkAvailableRooms', $data);
     }
 
 }
--- app/Room.php
+++ app/Room.php
@@ -3,8 +3,29 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class Room extends Model
 {
     //
+
+    public function getAvailableRooms($start_date, $end_date){
+
+    	$avalible_rooms = DB::table('rooms as r')
+    						->select('r.id', 'r.name')
+    						->whereRaw("
+    							 r.id NOT IN(
+    							 	select b.room_id FROM reservations b
+    							 	WHERE NOT(
+    							 		b.date_out < '{$start_date}' OR 
+    							 		b.date_in > '{$end_date}'
+    							 		)
+    							 	)
+    							")
+    							->orderBy("r.id")
+    							->get();
+
+    							return $avalible_rooms;
+
+    }
 }
